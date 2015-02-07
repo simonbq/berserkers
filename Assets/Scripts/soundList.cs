75650c9e8afc8bd3e92704c57b8d9168962019d8
Sebastian <a13sebja@student.his.se>
2015-02-07 16:10:57 +0100

    screenshakes

---
+++ Assets/Scripts/soundList.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundList : MonoBehaviour {
+	public AudioClip[] audioClips;
+	public bool playOnAwake = false;
+	// Use this for initialization
+	void Awake () {
+		if(playOnAwake) PlayRandom();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Play(int index) {
+		if (index >= audioClips.Length || index < 0) {
+			Debug.LogError ("invalid index : soundList.Play(int index)");		
+		} 
+
+		else {
+			audio.clip = audioClips [index];
+			audio.Play();
+		}
+	}
+	void Play() {
+		Play (0);
+	}
+
+	void PlayRandom() {
+		if (audioClips.Length < 1) {
+			Debug.LogError ("there are no audio clips in this list");
+		}
+		else {
+		audio.clip = audioClips [Random.Range (0, audioClips.Length)];
+		audio.Play ();
+		}
+	}
+}
