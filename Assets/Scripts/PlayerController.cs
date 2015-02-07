eed21ccb67d35d75f3750f58325b33cfda3da495
Simon Bergqvist <bergqvist.simon@gmail.com>
2015-02-07 13:06:50 +0100

    basic player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+	public float turnSpeed;
+	public Color playerColor;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = playerColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigidbody.MovePosition(transform.position + transform.forward * movementSpeed);
+
+		//transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + Input.GetAxis("Horizontal") * turnSpeed, transform.eulerAngles.z);
+
+		if(Input.GetKey (KeyCode.A)){
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 1 * turnSpeed, transform.eulerAngles.z);
+		}
+		if(Input.GetKey (KeyCode.D)){
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 1 * turnSpeed, transform.eulerAngles.z);
+		}
+	}
+}
