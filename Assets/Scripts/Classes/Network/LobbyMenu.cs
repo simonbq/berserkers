292de803a3eea8ee11bf16f5616973d8f7b3fbf3
tEFFx <erikkoii@gmail.com>
2015-02-07 14:03:52 +0100

    Hosting works!

---
+++ Assets/Scripts/Classes/Network/LobbyMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (!Connections.GetInstance ().isConnected) 
+		{
+			GUILayout.BeginHorizontal ();
+
+			if(GUILayout.Button ("Host"))
+			{
+				Connections.GetInstance().HostLobby(8);
+			}
+
+			GUILayout.Button ("Refresh");
+			GUILayout.EndHorizontal ();
+
+			GUILayout.BeginHorizontal ();
+			GUILayout.Label ("Nickname");
+			Connections.GetInstance ().localNickname = GUILayout.TextField (Connections.GetInstance ().localNickname, GUILayout.Width (300));
+			GUILayout.EndHorizontal ();
+		}
+
+		else
+		{
+			foreach(PlayerInfo p in Connections.GetInstance().players.Values)
+			{
+				GUILayout.Box(p.name + " | ID: " + p.id + " | Ready: " + p.ready);
+			}
+		}
+	}
+}
