6810ceb096caf27a528632ab2bc56a44f6cbca57
Sebastian <a13sebja@student.his.se>
2015-02-07 15:33:44 +0100

    screenshakes

---
+++ Assets/Scripts/screenshake.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenshake : MonoBehaviour {
+	public float intensity = 1.0f;
+	public float duration = 1.0f;
+	private float elapsed = 0.0f;
+	public AnimationCurve ac;
+	bool shaking = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (shaking) {
+			float force = ac.Evaluate(elapsed / duration) * intensity;
+			transform.position = Random.insideUnitSphere.normalized * force;
+
+			elapsed += Time.fixedDeltaTime;
+			if (elapsed > duration) {
+				transform.position = Vector3.zero;
+				shaking = false;
+			}
+		}
+	}
+
+	public void Shake() {
+		elapsed = 0.0f;
+		shaking = true;
+	}
+}
