468c615ac018af363b9924bd30e9becefd19c8cb
Ryxali <ryxali@gmail.com>
2015-02-08 23:52:33 +0100

    fiunale

--- Assets/Scripts/IntroScreen.cs
+++ Assets/Scripts/IntroScreen.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class IntroScreen : MonoBehaviour {
 	public float timeToAllowTransition = 1.5f;
 	public GameObject menu;
-	public MovieTexture movie;
+	public Texture[] movie;
+	private int current = 0;
+	public float loopTime = 2.0f;
+	private float timeStamp = 0.0f;
 	private Color col = Color.white;
+	private float timePerFrame;
 	bool locked = false;
 	// Use this for initialization
 	void Start () {
@@ -14,14 +18,18 @@ public class IntroScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		timePerFrame = loopTime / movie.Length;
 		if(!locked && Time.time > timeToAllowTransition && Input.anyKey) {
-			StartCoroutine(transition);
+			StartCoroutine(transition());
 		}
+		current = (int)(Time.time / timePerFrame) % movie.Length;
 	}
 
 	void OnGUI()
 	{
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movie);
+		if(movie.Length != 0) {
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movie[current]);
+		}
 	}
 
 	private IEnumerator transition() {
