dc7df8f16993f941975df3228fc5cf0a18bed59e
Ryxali <ryxali@gmail.com>
2015-02-10 01:47:12 +0100

    score screen icon smallified

---
+++ Assets/Scripts/Classes/MenuStates/GameMenuState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameMenuState : MenuState {
+	ActionData data;
+	private List<Button> buttons = new List<Button>();
+	public GameMenuState() {
+		data = new ActionData ();
+		Button b;
+
+		b = new Button ((int)Menu.SCREEN_SIZE.x / 2, 300, 256, 128, 0.0f, 0.0f);
+		b.setText("Disconnect");
+		b.setOnClick (ButtonActions.disconnect);
+	}
+
+	public override void update(Menu m) {
+		data.menu = m;
+	}
+
+	public override void render() {
+
+	}
+}
