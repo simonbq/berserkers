76c3a6e34cc0f4452a5dbf53fb284097b1fbec17
Sebastian <a13sebja@student.his.se>
2015-02-15 21:59:36 +0100

    finished credits and how to play

--- Assets/Scripts/Classes/MenuStates/CredState.cs
+++ Assets/Scripts/Classes/MenuStates/CredState.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CredState : MenuState {
+	private string text = "Rasmus Hofreus - Design\nSebastian Jarkeborn - Programming\nErik Thomasson Forsberg - Programming\nNiklas Lindblad - Programming\nSimon Bergqvist - Programming\nViktor Engstrom - Music\nVile Frans Kaathe - Sound, Voice Acting\nViktor Zryd - Sound, Voice Acting\nNiklas Bergwall - Animation, Voice Acting\nGustav Rosberg - Animation, Modeling\nMattias Lindblad - Modeling\nEvelina Waara - Animation, 2D Art";
+	private List<Button> buttons = new List<Button>();
+	private ActionData data;
+
+	public CredState() {
+		Button b;
+		b = new Button (960 - 128, 500, Button.BUTTON_NORM);
+		b.setText ("Back");
+		b.setOnClick (ButtonActions.back);
+		buttons.Add (b);
+		
+		data = new ActionData ();
+	}
+
 	public override void update(MenuBase m) {
+		data.menu = m;
+		foreach(Button b in buttons) {
+			b.calculate(data);
+		}
 	}
 
 	public override void render() {
-		GUI.Label(new Rect(Screen.width / 4,Screen.height / 4,(Screen.width / 4) * 3,(Screen.height / 4) * 3),"text\nhej");
+		GUI.Label(new Rect(Screen.width / 4,100,Screen.width,Screen.height),text);
+		foreach(Button b in buttons) {
+			b.update(data);
+		}
 	}
 }
--- Assets/Scripts/Classes/MenuStates/HowState.cs
+++ Assets/Scripts/Classes/MenuStates/HowState.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HowState : MenuState {
+	private string text = "BERZERKURUUU\n\nSteer your berserker right and left\nAvoid walls and rocks\nPick up flaming spheres to increase your speed\nDefeat your opponents by being faster when colliding\n\nDefault controls:\nPlayer 1: A-D\nPlayer 2: Left-Right\nPlayer 3: K-L\nPlayer 4: G-H";
+	private List<Button> buttons = new List<Button>();
+	private ActionData data;
+
+	public HowState() {
+		Button b;
+		b = new Button (960 - 128, 500, Button.BUTTON_NORM);
+		b.setText ("Back");
+		b.setOnClick (ButtonActions.back);
+		buttons.Add (b);
+
+		data = new ActionData ();
+	}
+
 	public override void update(MenuBase m) {
+		data.menu = m;
+		foreach(Button b in buttons) {
+			b.calculate(data);
+		}
 	}
 	
 	public override void render() {
-		GUI.Label(new Rect(Screen.width / 2 - 512,200,Screen.width,Screen.height),"text\nhej");
+		GUI.Label(new Rect(Screen.width / 4,100,Screen.width,Screen.height),text);
+		foreach(Button b in buttons) {
+			b.update(data);
+		}
 	}
 }
