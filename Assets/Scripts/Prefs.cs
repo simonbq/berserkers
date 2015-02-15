a73d0433d5678980c43dc12d4a00834b98251f97
Sebastian <a13sebja@student.his.se>
2015-02-15 17:17:17 +0100

    ljud och att man inte dör när man vunnit och grejer

---
+++ Assets/Scripts/Prefs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prefs : MonoBehaviour {
+
+	public static float GetVolume() {
+		return PlayerPrefs.GetFloat ("volume", 1f);
+	}
+
+	public static string GetName() {
+		return PlayerPrefs.GetString ("name", "Unnamed");
+	}
+
+	public static void SetVolume(float vol) {
+		PlayerPrefs.SetFloat ("volume", vol);
+	}
+
+	public static void SetName(string name) {
+		PlayerPrefs.SetString ("name", name);
+	}
+
+	public static void Save() {
+		PlayerPrefs.Save ();
+	}
+}
