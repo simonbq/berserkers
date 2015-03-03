937dfd2bfdb16ee9ce69de3d86789d2c46df6620
simonbq <bergqvist.simon@gmail.com>
2015-03-03 15:48:59 +0100

    made sure powerups dont spawn in lobby(?)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -190,7 +190,7 @@ public class GameController : MonoBehaviour {
 	}
 
 	void SpawnPowerUp(){
-        if (spawnPowerups)
+        if (spawnPowerups && Application.loadedLevelName!="Lobby")
         {
             int spawnsLeft = spawnPoints.Count - powerupSpawns.Count;
             int alive = Mathf.CeilToInt((float)playersAlive / 2);
